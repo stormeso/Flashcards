dc8f133d9b17e487f5103873f79f85728cd816a3
stormeso <torques.sophia@escola.pr.gov.br>
2025-05-30 00:57:15 +0000

    Criei funções e elementos no java

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
     'Matemática',
     'Qual é o valor de π (pi) aproximado?',
-    'O valor de π é aproximadamente 3,14'
+    'O valor de π é aproximadamente 3,14',
 )
 
 criaCartao(
     'História',
     'Quem foi o primeiro presidente do Brasil?',
-    'O primeiro presidente do Brasil foi Deodoro da Fonseca'
+    'O primeiro presidente do Brasil foi Deodoro da Fonseca',
 )
 
 criaCartao(
     'Ciências',
     'O que é fotossíntese?',
-    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar'
+    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar',
 )
