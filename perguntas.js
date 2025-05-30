dc1089372bbb887cd15f83709f4c842a8458b45b
stormeso <torques.sophia@escola.pr.gov.br>
2025-05-30 01:04:27 +0000

    Adicionei o resto das perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,59 @@
 criaCartao(
     'Matemática',
     'Qual é o valor de π (pi) aproximado?',
-    'O valor de π é aproximadamente 3,14',
+    'O valor de π é aproximadamente 3,14'
 )
 
 criaCartao(
     'História',
     'Quem foi o primeiro presidente do Brasil?',
-    'O primeiro presidente do Brasil foi Deodoro da Fonseca',
+    'O primeiro presidente do Brasil foi Deodoro da Fonseca'
 )
 
 criaCartao(
     'Ciências',
     'O que é fotossíntese?',
-    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar',
+    'Fotossíntese é o processo pelo qual as plantas produzem seu próprio alimento usando luz solar'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos jogadores tem um time de futebol?',
+    'Um time de futebol tem 11 jogadores'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu “Dom Casmurro”?',
+    'Machado de Assis escreveu “Dom Casmurro”'
+)
+
+criaCartao(
+    'Artes',
+    'Qual pintor ficou famoso por pintar girassóis?',
+    'Vincent van Gogh ficou famoso por pintar girassóis'
+)
+
+criaCartao(
+    'Música',
+    'Qual é o instrumento principal do samba?',
+    'O instrumento principal do samba é o pandeiro'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é DNA?',
+    'DNA é o material genético que contém as instruções para o funcionamento dos seres vivos'
+)
+
+criaCartao(
+    'Física',
+    'O que é gravidade?',
+    'Gravidade é a força que atrai os objetos para o centro da Terra'
+)
+
+criaCartao(
+    'Cinema',
+    'Quem dirigiu o filme “Titanic”?',
+    'O filme “Titanic” foi dirigido por James Cameron'
 )
